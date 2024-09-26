747207532fbcc3bef28879fcd33d7fbd591af66c
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2024-09-27 03:00:38 +0530

    Remove debug statements

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ export async function scraper(url, sort_type, search_query = "") {
         if (sort === undefined) {
             throw new Error(`Invalid sort value: ${sort_type}`);
         }
-        //console.debug(sort)
 
         //Create the URL
         var newurl = await listugcposts(url, sort, "", search_query);
-        //console.debug(`Constructed URL: ${newurl}`);
 
         //Get the reviews
         var response = await axios.get(newurl);
@@ -30,33 +28,27 @@ export async function scraper(url, sort_type, search_query = "") {
         }
         var data = response.data.split(")]}'")[1];
         var json = JSON.parse(data);
-        //console.debug(json);
 
         //Check if there are any reviews
         if (!json || json.length == 0) {
-            //console.debug("No reviews found.");
             return 0;
         }
 
         //Store the reviews in the reviews array
         var reviews = json[2];
-        //console.debug(reviews);
 
         //Check if there are any reviews on nextpage
         if (json[1] == null) {
-            //console.debug("No more reviews found.");
             return reviews; //Return the reviews if there are no more pages
         }
 
         //Get the next page
         var nextpage = json[1].replace(/"/g, "");
-        //console.debug(`Next page: ${nextpage}`);
 
         //Get the reviews on all the next pages until there are no more
         while (nextpage && nextpage !== "null") {
             //Create the URL
             var newurl = await listugcposts(url, sort, nextpage, search_query);
-            //console.debug(`Constructed URL: ${newurl}`);
 
             //Get the reviews
             var response = await axios.get(newurl);
@@ -65,19 +57,15 @@ export async function scraper(url, sort_type, search_query = "") {
             }
             var data = response.data.split(")]}'")[1];
             var json = JSON.parse(data);
-            //console.debug(json);
 
             //Store the reviews in the reviews array
             var reviews = reviews.concat(json[2]);
-            //console.debug(reviews);
 
             //Check if there are any reviews on nextpage
             if (json[1] == null) {
-                //console.debug("No more reviews found.");
                 return reviews;
             }
             var nextpage = json[1].replace(/"/g, "");
-            //console.debug(`Next page: ${nextpage}`);
 
             // Sleep for 1 second
             await new Promise(resolve => setTimeout(resolve, 1000));
